6dde46686af5eaea8f4524a99802422b7bf4f8df
Alecto <fomenkoandrey1978@gmail.com>
2024-03-01 21:19:34 +0200

    05 - reusable components

---
+++ 05-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import Button from './components/Button.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  )
+}
+
+export default App
---
+++ 05-react-app/src/components/Button.jsx
@@ -0,0 +1,12 @@
+const Button = () => {
+  return (
+    <div>
+      <button type="button" disabled>
+        Some text
+      </button>
+      <h2>disabled button</h2>
+    </div>
+  )
+}
+
+export default Button
