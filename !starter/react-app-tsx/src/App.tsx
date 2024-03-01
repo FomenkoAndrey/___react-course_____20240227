bf00982a9f630b025bf986529f41d169c39048d3
Alecto <fomenkoandrey1978@gmail.com>
2024-03-01 21:10:44 +0200

    starters JSX and TSX created

---
+++ !starter/react-app-tsx/src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>Hello_from_App_TSX_component</div>
+}
+
+export default App
---
+++ !starter/react-app/src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>Hello_from_App_component</div>
+}
+
+export default App
